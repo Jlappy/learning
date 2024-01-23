f7ec5c4b8a756d7fd61cfc3d82e9ab75183a4b6b
SieuNhanGaoXanh <kieuminhhoang2911@gmail.com>
2024-01-23 20:21:00 +0700

    serodd

---
+++ todo-list/client-todo/src/App.tsx
@@ -0,0 +1,14 @@
+import "./assets/scss/App.scss";
+import { useRoutes } from "react-router-dom";
+import routes from "./router/routes";
+import { SetAuthContextProvider } from "./contexts";
+
+export default function App() {
+  const router = useRoutes(routes);
+
+  return (
+    <div className="app-container">
+      <SetAuthContextProvider>{router}</SetAuthContextProvider>
+    </div>
+  );
+}
---
+++ todo-list/server-mongo/src/models/LinkCard.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const LinkCardSchema = new Schema({
+  title: {
+    type: String,
+    require: true,
+  },
+  url: {
+    type: String,
+    require: true,
+  },
+  status: {
+    type: String,
+    enum: ["TO LEARN", "LEARNING", "LEARNED"],
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("linkcards", LinkCardSchema);
